f8d38a44745fdc14d6e72154571cdb6f18362dce
Miyu Sato <miyuba0192@gmail.com>
2021-07-11 00:02:22 +0900

    “メモ追加機能作成”

--- SimpleMemoApp/AddMemoViewController.swift
+++ SimpleMemoApp/AddMemoViewController.swift
@@ -9,21 +9,44 @@ import UIKit
 
 class AddMemoViewController: UIViewController {
 
+    @IBOutlet weak var memoTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func save(_ sender: Any) {
+        //memoInputに入力したテキストを代入
+        var memoInput = memoTextView.text
+        //userdefaultsを用意
+        let ud = UserDefaults.standard
+        
+        //前にメモを入力したことがあれば元々入ってるuserdefaultsの値を取り出す
+        if ud.array(forKey: "memo") != nil {
+            //SavedMemoArrayにAny型からString型としてud.arrayから値を取り出して代入
+            var savedMemoArray = ud.array(forKey: "memo") as! [String]
+            //memoInputにメモが入ってるかどうか
+            if memoInput != nil {
+                savedMemoArray.append(memoInput!)
+            } else {
+                print("何も入力されていません")
+            }
+            //配列savedMemoを保存
+            ud.set(savedMemoArray, forKey: "memo")
+        //前にメモを入力したことがなければ
+        } else {
+            var newMemoArray = [String]()
+            //memoInputにメモが入ってるかどうか
+            if memoInput != nil {
+                newMemoArray.append(memoInput!)
+            } else {
+                print("何も入力されていません")
+            }
+            ud.set(newMemoArray, forKey: "memo")
+        }
+        //即時保存
+        ud.synchronize()
+        self.navigationController?.popViewController(animated: true)
     }
-    */
-
 }
--- SimpleMemoApp/ViewController.swift
+++ SimpleMemoApp/ViewController.swift
@@ -7,14 +7,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController ,UITableViewDelegate,UITableViewDataSource{
+  
+    @IBOutlet weak var memoTableView: UITableView!
+    
+    //String型の空の配列を作成
+    var memoArray = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        memoTableView.delegate = self
+        memoTableView.dataSource = self
+        
         
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        loadMemo()
+    }
 
+    //セルに何個表示させるか
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return memoArray.count
+    }
+    
+    //セルに表示させる内容
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //tavleViewCellの登録
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MemoCell")!
+        //cellのtextにmemoArrayを順番に入れる
+        cell.textLabel?.text = memoArray[indexPath.row]
+        //最後にcellを返してあげるよ！
+        return cell
+    }
+    
+    //セルを押した時に呼ばれる関数
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //identifierを使って画面遷移
+        self.performSegue(withIdentifier: "toDetail", sender: nil)
+        //おされたら押した状態を解除
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //userdefaultsからデータを呼び出す
+    func loadMemo(){
+        let ud = UserDefaults.standard
+        //メモが入っていたら
+        if ud.array(forKey: "memo") != nil {
+            memoArray = ud.array(forKey: "memo") as! [String]
+            //データを呼び出した後にtableViewをリロードしてくれる
+            memoTableView.reloadData()
+        }
+    }
 
+    //次の画面にデータを渡す
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //次の画面を取得
+        let detailViewController = segue.destination
+    }
+    
 }
 
