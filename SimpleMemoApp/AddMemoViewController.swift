39922aaf8a2e1eaf36845f4e11f02e6816fa4954
Miyu Sato <miyuba0192@gmail.com>
2021-07-10 10:42:57 +0900

    storyboardの作成

---
+++ SimpleMemoApp/AddMemoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddMemoViewController.swift
+//  SimpleMemoApp
+//
+//  Created by 佐藤未悠 on 2021/07/10.
+//
+
+import UIKit
+
+class AddMemoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
